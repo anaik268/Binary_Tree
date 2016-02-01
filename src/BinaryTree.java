21107caedcae5a79d2d41643a799796992f55dfe
Abhishek Naik <ANAIK@abhisheks-mbp.resource.campus.njit.edu>
2016-02-01 12:26:13 -0500

    Added Binary Class and renamed BinaryTreeImplementation name to BinaryTreeMain

---
+++ src/BinaryTree.java
@@ -0,0 +1,18 @@
+
+public class BinaryTree {
+	Node root;
+	private int nodeCount = 0;
+	
+	public void insert(int value)
+	{
+		
+	}
+	
+	public boolean isEmpty(){
+		return nodeCount == 0;
+	}
+	
+	public int size(){
+		return nodeCount;
+	}
+}
---
+++ src/BinaryTreeMain.java
@@ -0,0 +1,9 @@
+
+public class BinaryTreeMain {
+	public static void main(String[] args)
+	{
+		Node root = new Node(20);
+		System.out.println(root.getValue());
+	}
+	
+}
